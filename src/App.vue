0d02da05ba8e51de6341f0a95b5d19f8313d3d9b
Daniel VENERA <d.venera@groupeonepoint.com>
2021-11-06 10:10:47 +0100

    here map

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HereMap  :center="center" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HereMap from "./components/HereMap.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    HereMap,
   },
+  data(){
+    return {
+      center:{ 
+        lat: 40.730610, 
+        lng: -73.935242
+      }
+    }
+  }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
